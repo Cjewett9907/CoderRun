a6a728d551062019b0431fbbc65b0f6a69f86a45
Cjewett_work <Craig9907@gmail.com>
2019-01-21 07:14:27 -0800

    add html button functionality and onclick handlers for instructions and story

--- src/index.js
+++ src/index.js
@@ -11,12 +11,12 @@ document.addEventListener("DOMContentLoaded", () => {
   // const splashBackground = setInterval(bg.draw, 40);
   document.getElementById('play_text').innerHTML = 'PLAY'
   document.getElementById('title_text').innerHTML = 'CODER RUN'
-  document.getElementById('instructions_text').addEventListener('mouseover', () => {
+  document.getElementById('instructions_text').addEventListener('click', () => {
     instructions_text.innerHTML = 
         `After studying exessively for exams, Max is trapped in the nightmare world of Coder Run <br> <br>
          Help Max escape the gauntlet of buggy code!`})
   
-  document.getElementById('keys').addEventListener('mouseover', () => {
+  document.getElementById('keys').addEventListener('click', () => {
     keys.innerHTML = '<br> Use arrow keys or WASD to move, <br> pressing up twice performs a double jump. <br>  Bugs make you drowsy, Coffee wakes you up!'
   })
 
@@ -30,11 +30,20 @@ document.addEventListener("DOMContentLoaded", () => {
     game.start();
   })
 
-  // document.getElementById('diff').addEventListener('click', () => {   
-  //   document.getElementById('diff').style.visibility = 'hidden'
-  //   document.getElementById('diff-settings').style.visibility = 'visible'
-  
-  // })
+  document.getElementById('diff').addEventListener('click', () => {   
+    document.getElementById('diff').style.visibility = 'hidden'
+    var items = document.getElementsByClassName('diff-settings');
+      for (var i=0; i < items.length; i++) {
+        items[i].style.visibility = 'visible';
+      }  
+  })
+  document.getElementById('diff-div').addEventListener('click', () => {   
+    document.getElementById('diff').style.visibility = 'visible'
+    var items = document.getElementsByClassName('diff-settings');
+      for (var i=0; i < items.length; i++) {
+        items[i].style.visibility = 'hidden';
+      }  
+  })
 
  
   
